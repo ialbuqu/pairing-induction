e9555e34858262aac99f47e889f70e78a51f721b
Isabella Albuquerque <ialbuqu@thoughtworks.com>
2020-05-27 17:41:08 +0200

    Creating skeleton code for TicTacToe pairing exercise

---
+++ src/main/java/tictactoe/TicTacToe.java
@@ -0,0 +1,18 @@
+package tictactoe;
+
+/*
+ACCEPTANCE CRITERIA
+
+* a game has nine fields in a 3x3 grid
+* there are two players in the game (X and O)
+* players take turns taking fields until the game is over
+* a player can take a field if not already taken
+* a game is over when all fields are taken
+* a game is over when all fields in a row are taken by a player
+* a game is over when all fields in a column are taken by a player
+* a game is over when all fields in a diagonal are taken by a player
+
+ */
+
+public class TicTacToe {
+}
---
+++ src/test/java/tictactoe/TicTacToeTest.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TicTacToeTest {
+
+    @Test
+    public void isTestSetUpWorking() {
+        assertTrue(true);
+    }
+
+}
